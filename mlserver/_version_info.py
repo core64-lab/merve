890e734c395963b5d674822a7d113f8ceeb21ace
Alexander Herzog <alexander.herzog.23@ucl.ac.uk>
2025-10-21 17:37:49 +0100

    Added support to dynamically detect mlserver package installation from other sources (such as git) - additional override via MLSERVER_GIT_URL possible

--- mlserver/_version_info.py
+++ mlserver/_version_info.py
@@ -1,14 +1,14 @@
 # Auto-generated version information
 # This file is created during package build to embed git information
-# Generated at: 2025-09-18T15:20:43.044109Z
+# Generated at: 2025-10-15T19:57:07.537140Z
 # DO NOT EDIT MANUALLY - it will be overwritten during build
 
 VERSION = "0.2.0"
-GIT_COMMIT = "34e0d99"
+GIT_COMMIT = "514b9b0"
 GIT_TAG = ""
 GIT_BRANCH = "main"
 GIT_DIRTY = True
-BUILD_TIME = "2025-09-18T15:20:43.044118Z"
+BUILD_TIME = "2025-10-15T19:57:07.537430Z"
 
 # This file is populated during package build
 # These values are embedded at build time and available at runtime
